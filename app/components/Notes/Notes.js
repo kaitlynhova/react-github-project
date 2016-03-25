9766f280e18f37d3d8bd9214a04e64363e3b1532
Kaitlyn Hova <kaitlynhova@gmail.com>
2016-03-25 04:46:44 -0300

    new Notes.js es6 syntax

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -2,13 +2,8 @@ import React      from 'react';
 import NotesList  from'./NotesList';
 import AddNote    from './AddNote';
 
-var Notes = React.createClass({
-  propTypes:{
-    username: React.PropTypes.string.isRequired,
-    notes: React.PropTypes.array.isRequired,
-    addNote: React.PropTypes.func.isRequired
-  },
-  render: function(){
+class Notes extends React.Component {
+  render(){
     return (
       <div>
         <h3>Notes for {this.props.username}</h3>
@@ -17,6 +12,12 @@ var Notes = React.createClass({
       </div>
     )
   }
-});
+}
+
+Notes.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  notes: React.PropTypes.array.isRequired,
+  addNote: React.PropTypes.func.isRequired
+}
 
 module.exports = Notes;
